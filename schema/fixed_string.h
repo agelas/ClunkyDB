e128a40335a4f45aaaf2b747f94cd269301134f0
Mathias I <55096933+agelas@users.noreply.github.com>
2026-04-21 22:50:19 -0700

    Add trait + concept for FixedStringLike

--- schema/fixed_string.h
+++ schema/fixed_string.h
@@ -6,49 +6,53 @@
 
 namespace clunkydb::schema {
 
-template <std::size_t N>
-struct fixed_string {
-    char value[N]{};
+template <std::size_t N> struct fixed_string {
+  char value[N]{};
 
-    constexpr fixed_string(const char (&input)[N]) {
-        for (std::size_t i = 0; i < N; ++i) {
-            value[i] = input[i];
-        }
+  constexpr fixed_string(const char (&input)[N]) {
+    for (std::size_t i = 0; i < N; ++i) {
+      value[i] = input[i];
     }
+  }
 
-    [[nodiscard]] constexpr auto size() const -> std::size_t {
-        return N;
-    }
+  [[nodiscard]] constexpr auto size() const -> std::size_t { return N; }
 
-    [[nodiscard]] constexpr auto view() const -> std::string_view {
-        return std::string_view{value, N - 1};
-    }
+  [[nodiscard]] constexpr auto view() const -> std::string_view {
+    return std::string_view{value, N - 1};
+  }
 };
 
-template <std::size_t N>
-fixed_string(const char (&)[N]) -> fixed_string<N>;
+template <std::size_t N> fixed_string(const char (&)[N]) -> fixed_string<N>;
 
 template <std::size_t N, std::size_t M>
-[[nodiscard]] constexpr auto operator==(const fixed_string<N>& lhs,
-                                        const fixed_string<M>& rhs) -> bool {
-    if constexpr (N != M) {
+[[nodiscard]] constexpr auto operator==(const fixed_string<N> &lhs,
+                                        const fixed_string<M> &rhs) -> bool {
+  if constexpr (N != M) {
+    return false;
+  } else {
+    for (std::size_t i = 0; i < N; ++i) {
+      if (lhs.value[i] != rhs.value[i]) {
         return false;
-    } else {
-        for (std::size_t i = 0; i < N; ++i) {
-            if (lhs.value[i] != rhs.value[i]) {
-                return false;
-            }
-        }
-        return true;
+      }
     }
+    return true;
+  }
 }
 
 template <std::size_t N, std::size_t M>
-[[nodiscard]] constexpr auto operator!=(const fixed_string<N>& lhs,
-                                        const fixed_string<M>& rhs) -> bool {
-    return !(lhs == rhs);
+[[nodiscard]] constexpr auto operator!=(const fixed_string<N> &lhs,
+                                        const fixed_string<M> &rhs) -> bool {
+  return !(lhs == rhs);
 }
 
-}  // namespace clunkydb::schema
+template <typename T> struct is_fixed_string : std::false_type {};
 
-#endif  // CLUNKYDB_SCHEMA_FIXED_STRING_H_
+template <std::size_t N>
+struct is_fixed_string<fixed_string<N>> : std::true_type {};
+
+template <typename T>
+concept FixedStringLike = is_fixed_string<std::remove_cvref_t<T>>::value;
+
+} // namespace clunkydb::schema
+
+#endif // CLUNKYDB_SCHEMA_FIXED_STRING_H_
