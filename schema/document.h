84305c73a8dde085744bea2b5beaf7ae9000f4cb
Mathias I <55096933+agelas@users.noreply.github.com>
2026-04-21 22:49:59 -0700

    Simplify FieldSpec concept a little...or at least make it easier to reason about

--- schema/document.h
+++ schema/document.h
@@ -9,38 +9,35 @@
 
 namespace clunkydb::schema {
 
-template <fixed_string Name, typename T>
-struct field {
-    static constexpr auto name = Name;
-    using value_type = T;
+template <fixed_string Name, typename T> struct field {
+  static constexpr auto name = Name;
+  using value_type = T;
 };
 
-template <typename... Fields>
-struct document {
-    using fields = std::tuple<Fields...>;
-    static constexpr auto field_count = sizeof...(Fields);
+template <typename... Fields> struct document {
+  using fields = std::tuple<Fields...>;
+  static constexpr auto field_count = sizeof...(Fields);
 };
 
 template <typename T>
 concept FieldSpec = requires {
-    T::name;
-    typename T::value_type;
-} && std::same_as<decltype(T::name), const decltype(T::name)>;
+  T::name;
+  typename T::value_type;
+} && FixedStringLike<decltype(T::name)>;
 
 namespace detail {
 
-template <typename T>
-struct is_document : std::false_type {};
+template <typename T> struct is_document : std::false_type {};
 
 template <typename... Fields>
 struct is_document<document<Fields...>>
     : std::bool_constant<(FieldSpec<Fields> && ...)> {};
 
-}  // namespace detail
+} // namespace detail
 
 template <typename T>
 concept DocumentSchema = detail::is_document<std::remove_cvref_t<T>>::value;
 
-}  // namespace clunkydb::schema
+} // namespace clunkydb::schema
 
-#endif  // CLUNKYDB_SCHEMA_DOCUMENT_H_
+#endif // CLUNKYDB_SCHEMA_DOCUMENT_H_
