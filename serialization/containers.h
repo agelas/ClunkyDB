f2d777414e7727ba70969aa80dafcfc3ce36cf7f
Mathias I <55096933+agelas@users.noreply.github.com>
2026-04-29 22:57:34 -0700

    Add serializer for vector

--- serialization/containers.h
+++ serialization/containers.h
@@ -6,6 +6,7 @@
 #include <type_traits>
 #include <vector>
 
+#include "serialization/primitives.h"
 #include "serialization/serializer.h"
 
 namespace clunkydb::serialization {
@@ -20,9 +21,30 @@ template <typename T> inline constexpr bool is_vector_v = is_vector<T>::value;
 template <typename T>
   requires is_vector_v<T>
 struct Serializer<T> {
-  static void serialize(const T &value, std::vector<std::byte> &output) {}
+  static void serialize(const T &value, std::vector<std::byte> &output) {
+    Serializer<std::size_t>::serialize(value.size(), output);
+
+    for (const auto &element : value) {
+      Serializer<std::remove_cvref_t<decltype(element)>>::serialize(element,
+                                                                    output);
+    }
+  }
 
   static auto deserialize(std::span<const std::byte> input,
-                          std::size_t &offset) -> T {}
+                          std::size_t &offset) -> T {
+    auto size = Serializer<std::size_t>::deserialize(input, offset);
+
+    T result;
+    result.reserve(size);
+
+    for (std::size_t i = 0; i < size; ++i) {
+      auto element =
+          Serializer<std::remove_cvref_t<typename T::value_type>>::deserialize(
+              input, offset);
+      result.push_back(element);
+    }
+
+    return result;
+  }
 };
 } // namespace clunkydb::serialization
