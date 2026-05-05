55f71cc24ad3c9733ac6e81b67bd6ea0bf0ecada
Mathias I <55096933+agelas@users.noreply.github.com>
2026-05-04 23:28:33 -0700

    Create application specific test file

---
+++ serialization/domain_serialization_test.cpp
@@ -0,0 +1,16 @@
+#include <casssert>
+#include <cstddef>
+#include <span>
+#include <vector>
+
+#include "budget/types.h"
+#include "serialization/domain.h"
+#include "serialization/primitives.h"
+#include "serialization/serializer.h"
+
+namespace budget = clunkydb::budget;
+namespace serialization = clunkydb::serialization;
+
+static_assert(serialization::Serializable<budget::Allocation>);
+
+int main() { return 0; }
