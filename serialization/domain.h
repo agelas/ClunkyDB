86100b6a2c203359bb1574d7fbcfc0e7d7042c82
Mathias I <55096933+agelas@users.noreply.github.com>
2026-05-11 22:52:30 -0700

    Add serializer for ExpenseItem

--- serialization/domain.h
+++ serialization/domain.h
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <span>
 #include <stdexcept>
+#include <string>
 #include <type_traits>
 #include <vector>
 
@@ -10,6 +11,7 @@
 #include "serialization/containers.h"
 #include "serialization/primitives.h"
 #include "serialization/serializer.h"
+#include "serialization/string.h"
 
 namespace clunkydb::serialization {
 using budget::Allocation;
@@ -27,6 +29,31 @@ template <> struct Serializer<Allocation> {
     value.essential = Serializer<double>::deserialize(input, offset);
     value.non_essential = Serializer<double>::deserialize(input, offset);
     value.savings = Serializer<double>::deserialize(input, offset);
+    return value;
+  }
+};
+
+using budget::ExpenseItem;
+using budget::ExpenseType;
+template <> struct Serializer<ExpenseItem> {
+  static void serialize(const ExpenseItem &value,
+                        std::vector<std::byte> &output) {
+    Serializer<ExpenseType>::serialize(value.type, output);
+    Serializer<std::string>::serialize(value.name, output);
+    Serializer<double>::serialize(value.cost, output);
+    Serializer<std::string>::serialize(value.category, output);
+    Serializer<bool>::serialize(value.recurring, output);
+  }
+
+  static auto deserialize(std::span<const std::byte> input,
+                          std::size_t &offset) -> ExpenseItem {
+    ExpenseItem value{};
+    value.type = Serializer<ExpenseType>::deserialize(input, offset);
+    value.name = Serializer<std::string>::deserialize(input, offset);
+    value.cost = Serializer<double>::deserialize(input, offset);
+    value.category = Serializer<std::string>::deserialize(input, offset);
+    value.recurring = Serializer<bool>::deserialize(input, offset);
+
     return value;
   }
 };
