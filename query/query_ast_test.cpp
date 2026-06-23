85455f30b366cdabb5996476ba5602717d43c739
Mathias I <55096933+agelas@users.noreply.github.com>
2026-06-22 19:37:17 -0700

    Add static assert for intended public API

--- query/query_ast_test.cpp
+++ query/query_ast_test.cpp
@@ -7,5 +7,6 @@ namespace schema = clunkydb::schema;
 namespace query = clunkydb::query;
 
 static_assert(query::field_ref<"name">::name == schema::fixed_string{"name"});
+static_assert(query::field<"name">.name == schema::fixed_string{"name"});
 
 int main() { return 0; }
