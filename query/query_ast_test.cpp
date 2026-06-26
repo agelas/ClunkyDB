852e888334f8f2dcde6cde4354369d50505f7cd9
Mathias I <55096933+agelas@users.noreply.github.com>
2026-06-25 22:53:24 -0700

    Compile-time type checking static assert for query_ast

--- query/query_ast_test.cpp
+++ query/query_ast_test.cpp
@@ -1,6 +1,8 @@
 #include <cassert>
+#include <concepts>
 
 #include "query/field_ref.h"
+#include "query/query_ast.h"
 #include "schema/fixed_string.h"
 
 namespace schema = clunkydb::schema;
@@ -8,5 +10,9 @@ namespace query = clunkydb::query;
 
 static_assert(query::field_ref<"name">::name == schema::fixed_string{"name"});
 static_assert(query::field<"name">.name == schema::fixed_string{"name"});
+
+using Expected = query::eq<query::field_ref<"PaycheckNum">, int>;
+static_assert(
+    std::same_as<decltype(query::field<"PaycheckNum"> == 42), Expected>);
 
 int main() { return 0; }
