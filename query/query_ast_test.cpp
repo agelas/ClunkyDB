42f8546bb19be35ba3959635d16e446b0f59678b
Mathias I <55096933+agelas@users.noreply.github.com>
2026-07-11 23:24:25 -0700

    Add composition test in query_ast_test

--- query/query_ast_test.cpp
+++ query/query_ast_test.cpp
@@ -15,4 +15,10 @@ using Expected = query::eq<query::field_ref<"PaycheckNum">, int>;
 static_assert(
     std::same_as<decltype(query::field<"PaycheckNum"> == 42), Expected>);
 
+using Left = query::eq<query::field_ref<"PaycheckNum">, int>;
+using Right = query::eq<query::field_ref<"Date">, const char *>;
+static_assert(std::same_as<decltype((query::field<"PaycheckNum"> == 42) &&
+                                    (query::field<"Date"> == "2026-06-22")),
+                           query::logical_and<Left, Right>>);
+
 int main() { return 0; }
