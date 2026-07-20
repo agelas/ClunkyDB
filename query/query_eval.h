6daea7606cb67f5b305241445e2983bd8694d71e
Mathias I <55096933+agelas@users.noreply.github.com>
2026-07-19 21:57:10 -0700

    Start on query.h

--- query/query_eval.h
+++ query/query_eval.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "query/field_accessor.h"
+#include "query/field_ref.h"
+#include "query/query_ast.h"
+#include "schema/fixed_string.h"
+
+namespace clunkydb::query {
+
+template <schema::fixed_string<Name>, typename Value, typename Doc>
+constexpr auto eval(const eq<field_ref<Name>, Value> &query,
+                    const Doc &doc) -> bool {
+  return eval(query.lhs, doc) && eval(query.rhs, doc);
+}
+} // namespace clunkydb::query
