ec0c66596c10ee213a0c8f1ddab9f702abdbadf2
Mathias I <55096933+agelas@users.noreply.github.com>
2026-08-17 22:36:06 -0700

    Add `has_field_named_v` to field_lookup

--- schema/field_lookup.h
+++ schema/field_lookup.h
@@ -10,4 +10,7 @@ template <typename Schema, fixed_string Name> struct has_field_named;
 template <fixed_string Name, typename... Fields>
 struct has_field_named<document<Fields...>, Name>
     : std::bool_constant<((Fields::name == Name) || ...)> {};
+
+template <typename Schema, fixed_string Name>
+inline constexpr bool has_field_named_v = has_field_named<Schema, Name>::value;
 } // namespace clunkydb::schema
