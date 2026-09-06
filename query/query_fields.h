b9720e52ca0c3a6a92198fff9835fd9f9d84f78b
Mathias I <55096933+agelas@users.noreply.github.com>
2026-09-06 10:46:37 -0700

    Add AST templates

--- query/query_fields.h
+++ query/query_fields.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "query/query_ast.h"
+#include "schema/fixed_string.h"
 
 namespace clunkydb::query {
 template <typename... Types> struct type_list {};
@@ -16,4 +17,24 @@ template <typename Lhs, typename Rhs>
 using concat_t = typename concat<Lhs, Rhs>::type;
 
 template <typename Query> struct referenced_fields;
+
+template <schema::fixed_string Name, typename Value>
+struct referenced_fields<eq<field_ref<Name>, Value>> {
+  using type = type_list<field_ref<Name>>;
+};
+
+template <typename Lhs, typename Rhs>
+struct referenced_fields<logical_and<Lhs, Rhs>> {
+  using type = concat_t<typename referenced_fields<Lhs>::type,
+                        typename referenced_fields<Rhs>::type>;
+};
+
+template <typename Lhs, typename Rhs>
+struct referenced_fields<logical_or<Lhs, Rhs>> {
+  using type = concat_t<typename referenced_fields<Lhs>::type,
+                        typename referenced_fields<Rhs>::type>;
+};
+
+template <typename Query>
+using referenced_fields_t = typename referenced_fields<Query>::type;
 } // namespace clunkydb::query
